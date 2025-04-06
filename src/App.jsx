a34f523eb6926153fa9b865c7b04e9c2ceb21224
Rebecca Ayodele <rebeccaayodele08@gmail.com>
2025-04-06 04:21:40 +0100

    Added login, sign in and dashboard react-router

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
+import Auth from './layouts/Auth'
+import LoginPage from './pages/login'
+import Regsiter from './pages/register'
+import Dashboard from './pages/Dashboard'
+
+function App() {
+  const _route = createBrowserRouter(
+    [
+      {
+        path: "/auth",
+        element: <Auth />,
+        children: [
+          {
+            path: "login",
+            element: <LoginPage />
+          },
+          {
+            path: "register",
+            element: <Regsiter />
+          },
+          {
+            path: "dashboard",
+            element: <Dashboard />
+          }
+        ]
+      },
+      { path: "", element: <Navigate to="/auth/login" />  },
+      { path: "*", element: <div>404 page not found it seems you are lost</div> },
+    ]
+  )
+  return <RouterProvider router={_route} />
+}
+
+export default App
